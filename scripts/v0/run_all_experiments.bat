2e5b002fe1425a91914458f089cbc95ed0972982
JuanArizaCoding <juanarizaoonecha@gmail.com>
2025-02-15 13:49:49 +0100

    Removed unnecesary batch files

--- scripts/v0/run_all_experiments.bat
+++ scripts/v0/run_all_experiments.bat
@@ -5,10 +5,10 @@ call ..\date.bat
 for /f "tokens=*" %%F in ('powershell -command "echo $env:DATETIME"') do set DATETIME=%%F
 set IMAGES_FOLDER=images---%DATETIME%
 echo Carpeta creada: %IMAGES_FOLDER%
-goto :fin
+
 echo *EJECUTANDO: Backtracking vs Branch_AndBound_2...
 call run_experiment.bat bruteForce branchAndBound_2 Backtracking-BranchAndBound2 %IMAGES_FOLDER%
-:fin
+
 echo *EJECUTANDO: BranchAndBound_1 vs BranchAndBound_2...
 call run_experiment.bat branchAndBound_1 branchAndBound_2 BranchAndBound1-BranchAndBound2 %IMAGES_FOLDER%
 
