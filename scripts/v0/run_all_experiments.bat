cbc3ea861ea5b0af191ab963444bde9b743a8460
JuanArizaCoding <juanarizaoonecha@gmail.com>
2025-03-07 20:09:25 +0100

    Added scritping in v1, heuristic files and tables to visualize

--- scripts/v0/run_all_experiments.bat
+++ scripts/v0/run_all_experiments.bat
@@ -1,16 +1,19 @@
 @echo off
-call ..\date.bat
+
+echo *EJECUTANDO: Backtracking...
+call run_experiment.bat bruteForce
+echo *EJECUTANDO: branchAndBound_1...
+call run_experiment.batbranchAndBound_1
+echo *EJECUTANDO: branchAndBound_2...
+call run_experiment.bat branchAndBound_2
 
 :: Obtener la fecha desde la variable de entorno
+call ..\date.bat
 for /f "tokens=*" %%F in ('powershell -command "echo $env:DATETIME"') do set DATETIME=%%F
 set IMAGES_FOLDER=images---%DATETIME%
-echo Carpeta creada: %IMAGES_FOLDER%
 
-echo *EJECUTANDO: Backtracking vs Branch_AndBound_2...
-call run_experiment.bat bruteForce branchAndBound_2 Backtracking-BranchAndBound2 %IMAGES_FOLDER%
-
-echo *EJECUTANDO: BranchAndBound_1 vs BranchAndBound_2...
-call run_experiment.bat branchAndBound_1 branchAndBound_2 BranchAndBound1-BranchAndBound2 %IMAGES_FOLDER%
+call ..\postprocessing\postprocessing.bat bruteForce branchAndBound_1 Backtracking-BranchAndBound2 %IMAGES_FOLDER% v0 0
+call ..\postprocessing\postprocessing.bat branchAndBound_1 branchAndBound_2 BranchAndBound1-BranchAndBound2 %IMAGES_FOLDER% v0 0
 
 echo TODOS LOS EXPERIMENTOS FINALIZADOS.
 pause
